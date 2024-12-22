2ddcd40951d182144ea97d767f4df4b0956cb8b9
ota-fayz <ota_2411@mail.ru>
2024-12-22 18:26:32 +0100

    feat: base.sh & files

--- launch_scripts/base.sh
+++ launch_scripts/base.sh
@@ -3,16 +3,16 @@
 # 3) task addition
 
 python finetune.py \
---data-location=/path/to/datasets/ \
---save=/path/to/save/ \
+--data-location=./data \
+--save=./checkpoints \
 --batch-size=32 \
 --lr=1e-4 \
 --wd=0.0
 
 python eval_single_task.py \
---data-location=/path/to/datasets/ \
---save=/path/to/save/
+--data-location=./data \
+--save=./checkpoints \
 
 python eval_task_addition.py \
---data-location=/path/to/datasets/ \
---save=/path/to/save/
+--data-location=./data \
+--save=./checkpoints
